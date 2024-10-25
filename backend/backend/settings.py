00f05be634f05985ccdb9c731daee5809506c179
khuongduy-hoang <hoangduy00987@gmail.com>
2024-10-25 23:14:29 +0700

    update port

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -166,9 +166,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         
     ),
-    
+
     
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -212,7 +213,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND =  os.getenv('EMAIL_BACKEND')
 EMAIL_HOST =  os.getenv('EMAIL_HOST')
-EMAIL_PORT =  int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT =  os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS =  os.getenv('EMAIL_USE_TLS')
 EMAIL_HOST_USER =  os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD =  os.getenv('EMAIL_HOST_PASSWORD')
