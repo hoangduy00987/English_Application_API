00b82ad5c9861233586750265f3101024ccc6b18
khuongduy-hoang <hoangduy00987@gmail.com>
2024-10-25 21:11:33 +0700

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,8 +156,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),  # địa chỉ máy chủ cơ sở dữ liệu
-        # 'HOST':'localhost',
+        # 'HOST': os.environ.get('DB_HOST'),  # địa chỉ máy chủ cơ sở dữ liệu
+        'HOST':'localhost',
         'PORT': os.environ.get('DB_PORT'),  # cổng cơ sở dữ liệu
     }
 }
@@ -203,6 +203,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'  # Đường dẫn truy cập tệp tĩnh
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
