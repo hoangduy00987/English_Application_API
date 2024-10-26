e553c61012ff556411f41721422182c5544e2be8
khuongduy-hoang <hoangduy00987@gmail.com>
2024-10-26 14:30:59 +0700

    update port

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,6 +69,7 @@ CORS_ALLOWED_ORIGINS = [
    'http://localhost:3000', 
    'http://127.0.0.1:3000', 
    'http://engina.duytech.site',
+   'http://api.duytech.site',
    'http://localhost:3007', 
    'http://localhost:3008', 
    'http://localhost:3018',
