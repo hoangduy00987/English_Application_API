663a1fc2c1120e5f0399ba38daa70de3ec1640f7
khuongduy-hoang <hoangduy00987@gmail.com>
2024-11-16 22:06:48 +0700

    setting celery

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -11,20 +11,22 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'update-review-status-every-day': {
         'task': 'api.vocabulary.tasks.update_review_status',
-        'schedule': timedelta(days=1),
+        'schedule': timedelta(days=1),  # Chạy mỗi ngày
     },
     'send-reminder-email-every-day': {
         'task': 'api.login.tasks.send_reminder_email',
-        'schedule': timedelta(minutes=5),
+        'schedule': timedelta(minutes=5),  # Chạy mỗi 5 phút
     },
     'check-expo-tokens-weekly': {
         'task': 'api.login.tasks.periodic_token_check',
-        'schedule': crontab(day_of_week=1, hour=0, minute=0),
-    },'reset-weekly-points': {
+        'schedule': crontab(day_of_week=1, hour=0, minute=0),  # Chạy mỗi tuần vào thứ 2 lúc 00:00
+    },
+    'reset-weekly-points': {
         'task': 'api.vocabulary.tasks.reset_week_leaderboard_points',  
-        'schedule': crontab(minute=59, hour=23, day_of_week=6), 
-    },'reset-monthly-points': {
+        'schedule': crontab(minute=59, hour=23, day_of_week=6),  # Chạy mỗi tuần vào lúc 11:59 PM thứ 7
+    },
+    'reset-monthly-points': {
         'task': 'api.vocabulary.tasks.reset_month_leaderboard_points',  
-        'schedule': crontab(minute=59, hour=23, day=28), 
+        'schedule': crontab(minute=59, hour=23, day_of_month=28),  # Chạy vào ngày 28 mỗi tháng lúc 11:59 PM
     },
 }
